647fd85e7d51c780b9069dfcdb9886ab411e2a1d
Vinay D <vinayd@nvidia.com>
2021-05-04 21:50:25 +0530

    Implemented repairing ISBN code. Optimization remains

--- isbn.cpp
+++ isbn.cpp
@@ -23,8 +23,33 @@ bool check_isbn(unsigned char* code) {
   }
 }
 
+void repair_isbn(unsigned char* input_code) {
+
+  if (check_isbn(input_code)) {
+    printf("Code is already correct\n");
+    return;
+  }
+
+  for (int fault_position = 0; fault_position < ISBN_LENGTH - 1; fault_position++) {
+    unsigned char save = input_code[fault_position];
+    for (unsigned char digit = 0; digit < 10; digit++) {
+      input_code[fault_position] = digit;
+      if (check_isbn(input_code)) {
+        printf("Fault position %d -> Corrected code: %d%d%d%d%d%d%d%d%d%d\n",
+               fault_position,
+               int(input_code[0]), int(input_code[1]), int(input_code[2]),
+               int(input_code[3]), int(input_code[4]), int(input_code[5]),
+               int(input_code[6]), int(input_code[7]), int(input_code[8]),
+               int(input_code[9]));
+      }
+    }
+    input_code[fault_position] = save;
+  }
+}
+
 int main(int argc, char* argv[]) {
-  unsigned char isbn_code[ISBN_LENGTH] = {0, 1, 4, 0, 3, 1, 8, 1, 9, 4};
-  printf("Is the ISBN correct? %s\n", check_isbn(isbn_code) ? "Yes" : "No");
+  unsigned char isbn_code[ISBN_LENGTH] = {0, 1, 4, 0, 3, 1, 8, 5, 9, 4};
+  // printf("Is the ISBN correct? %s\n", check_isbn(isbn_code) ? "Yes" : "No");
+  repair_isbn(isbn_code);
   return 0;
 }
